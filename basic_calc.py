4d49b1d5e1430851fbb63b8dc889779c1a1e83b7
Miaxrz <ameeralakhani@gmail.com>
2025-04-26 12:35:16 +0400

    Added new file

---
+++ basic_calc.py
@@ -0,0 +1,26 @@
+num_1 = int(input('Enter the first number: '))
+num_2 = int(input('Enter the second number: '))
+
+#addition
+print('Addition of {} + {} = '.format(num_1,num_2))
+print(num_1 + num_2)
+
+#subtraction
+print('Subtraction of {} - {} = '.format(num_1,num_2))
+print(num_1 - num_2)
+
+#multiplication
+print('Multiplication of {} * {} = '.format(num_1,num_2))
+print(num_1 * num_2)
+
+#division
+print('Division of {} / {} = '.format(num_1,num_2))
+print(num_1 / num_2)
+
+#floor division
+print('Floor division of {} // {} = '.format(num_1,num_2))
+print(num_1 // num_2)
+
+#modulo
+print('Modulus of {} % {} = '.format(num_1,num_2))
+print(num_1 % num_2)
