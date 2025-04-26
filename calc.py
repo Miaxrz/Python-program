cd49e43a70ab31d1b7e22fdb4118ab01d183d9f4
Miaxrz <ameeralakhani@gmail.com>
2025-04-26 12:16:28 +0400

    Added a new feature to the file

--- calc.py
+++ calc.py
@@ -19,3 +19,14 @@ n2 = int(input('Enter second number: '))
 
 result = calculate(n1,n2,op)
 print(n1,op,n2,'=',result)
+
+continue_calculating = True
+while continue_calculating is True:
+       n1 = int(input('Enter first number: '))
+       op = input('Enter operator(+,-,*,/,^): ')
+       n2 = int(input('Enter second number: '))
+       result = calculate(n1,n2,op)
+       print(n1,op,n2,'=',result)
+       yes_or_no = input('Continue? (y/n): ')
+       if yes_or_no == 'n':
+            continue_calculating = False
