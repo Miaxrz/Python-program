fbb012369f5ba22fd8fb175fe58fef2888a29911
Ameera <ameeralakhani@gmail.com>
2025-04-26 12:45:07 +0400

    Update basic_calc.py
    Added a function with a few lines

--- basic_calc.py
+++ basic_calc.py
@@ -24,3 +24,17 @@ print(num_1 // num_2)
 #modulo
 print('Modulus of {} % {} = '.format(num_1,num_2))
 print(num_1 % num_2)
+
+# Function to perform the arithmetic operation: Addition
+def add(num_1, num_2):
+   return num_1 + num_2
+# Function to perform the arithmetic operation: Subtraction
+def subtract(num_1, num_2):
+   return num_1 - num_2
+# Function to perform the arithmetic operation: Multiplication
+def multiply(num_1, num_2):
+   return num_1 * num_2
+# Function to perform the arithmetic operation: Division
+def divide(num_1, num_2):
+   return num_1 / num_2
+print("Hi, This is the Basic console based calculator!\n")
