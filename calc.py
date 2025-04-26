223a017c21c342076a1cfc5052d1a674dbd1e195
Miaxrz <ameeralakhani@gmail.com>
2025-04-26 11:31:06 +0400

    Made some new changes to the file

--- calc.py
+++ calc.py
@@ -1,8 +1,3 @@
-n1 = int(input('Enter first number: '))
-op = input('Enter operator(+,-,*,/,^): ')
-n2 = int(input('Enter second number: '))
-print(n1,op,n2)
-
 def calculate(n1,n2,op):
     if op == '+':
         result = n1+n2
@@ -18,3 +13,9 @@ def calculate(n1,n2,op):
         print('You did not mention it right! Please check again.')
 
 
+n1 = int(input('Enter first number: '))
+op = input('Enter operator(+,-,*,/,^): ')
+n2 = int(input('Enter second number: '))
+print(n1,op,n2)
+result = calculate(n1,n2,op)
+print(result)
