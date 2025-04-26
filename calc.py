97750c77f3156195b9994d0cac7aaa798e3f3423
Miaxrz <ameeralakhani@gmail.com>
2025-04-26 11:25:02 +0400

    Added new lines to the end

--- calc.py
+++ calc.py
@@ -1,2 +1,20 @@
-inp = float(input('Welcome, please enter a number '))
-print('You entered:',inp)
+n1 = int(input('Enter first number: '))
+op = input('Enter operator(+,-,*,/,^): ')
+n2 = int(input('Enter second number: '))
+print(n1,op,n2)
+
+def calculate(n1,n2,op):
+    if op == '+':
+        result = n1+n2
+    elif op == '-':
+        result = n1-n2
+    elif op == '*':
+        result = n1*n2
+    elif op == '/':
+        result = n1/n2
+    elif op == '^':
+        result = n1**n2
+    else:
+        print('You did not mention it right! Please check again.')
+
+
